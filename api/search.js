af5124aa27e539ac53b7945a34303c74d6244105
alfan-irsyadi <irsyadialfan2@gmail.com>
2025-09-22 19:36:52 +0700

    fix search 2

--- api/search.js
+++ api/search.js
@@ -2,45 +2,7 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 import cors from "cors";
 
-const allowedOrigins = [
-    "http://localhost:5173",
-    "https://kemenag-batubara.vercel.app",
-    "https://backend-kemenag-batubara.vercel.app"
-];
-
-const corsMiddleware = cors({
-    origin: function (origin, callback) {
-        if (!origin) return callback(null, true);
-        if (allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            const isAllowed = allowedOrigins.some(allowedOrigin => 
-                origin.endsWith(allowedOrigin.replace("https://", "").replace("http://", ""))
-            );
-            if (isAllowed) {
-                callback(null, true);
-            } else {
-                callback(new Error("Not allowed by CORS"));
-            }
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
-});
-
 export default async function handler(req, res) {
-    // Apply CORS middleware
-    await new Promise((resolve, reject) => {
-        corsMiddleware(req, res, (err) => {
-            if (err) reject(err);
-            else resolve();
-        });
-    });
-
-    if (req.method !== 'GET') {
-        return res.status(405).json({ error: "Method not allowed" });
-    }
-
     try {
         const keyword = req.query.keyword;
         if (!keyword) {
